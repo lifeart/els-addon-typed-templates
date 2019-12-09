b0d2c21c70a3baea5c324363933e55a1b5086d10
Aleksandr Kanunnikov <lifeart92@gmail.com>
2019-12-09 10:49:03 +0300

    common types

--- src/lib/common-types.d.ts
+++ src/lib/common-types.d.ts
@@ -4,4 +4,12 @@ declare module '@glimmer/component' {
     }
 }
 
-type helperFn = ([...any], any) => any
+declare module "@ember/component/helper" {
+    export function helper<T>(T): T;
+}
+declare module "@ember/modifier" {
+    export function setModifierManager<T,U>(T,U): U;
+}
+declare module "@ember/component" {
+    export function setComponentTemplate<T,U>(T,U): U;
+}
