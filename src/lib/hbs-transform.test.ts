30098b1efea6b3f9613cc75eb4b316bdc4360558
Aleksandr Kanunnikov <lifeart92@gmail.com>
2020-06-05 20:28:29 +0300

    keyForItem test

--- src/lib/hbs-transform.test.ts
+++ src/lib/hbs-transform.test.ts
@@ -1,4 +1,4 @@
-import { positionForItem } from './hbs-transform';
+import { positionForItem, keyForItem } from './hbs-transform';
 
 describe('positionForItem', () => {
     it('return correct position from given location', ()=>{
@@ -8,5 +8,17 @@ describe('positionForItem', () => {
                 end: {line: 2, column: 5}
             }
         })).toEqual('0,3:2,5');
+    });
+});
+
+describe('keyForItem', () => {
+    it('return correct key from given node', ()=>{
+        expect(keyForItem({
+            loc: {
+                start: {line: 0, column: 3},
+                end: {line: 2, column: 5}
+            },
+            type: 'FooBar'
+        })).toEqual('0,3:2,5 - FooBar');
     });
 });
