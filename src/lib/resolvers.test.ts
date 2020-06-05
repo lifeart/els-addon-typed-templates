8be15124e83976534635196a010f5f153299825c
Aleksandr Kanunnikov <lifeart92@gmail.com>
2020-06-05 20:37:20 +0300

    fix tests

--- src/lib/resolvers.test.ts
+++ src/lib/resolvers.test.ts
@@ -3,5 +3,7 @@ import { relativeImport } from './resolvers';
 describe('relativeImport', () => {
     it('works', ()=>{
         expect(relativeImport('/foo/bar/baz', '/foo/bar/baz/boo.js')).toEqual('boo');
+        expect(relativeImport('/foo/bar/baz', '/foo/bar/baz/boo.ts')).toEqual('boo');
+
     });
 });
